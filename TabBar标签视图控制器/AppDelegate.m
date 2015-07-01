159d4f253bde5330be3a04991243999a3eefdf63
Lan <qs69m2s@163.com>
2015-07-01 22:49:36 +0800

    2015年07月01日22:49:33

--- TabBar标签视图控制器/AppDelegate.m
+++ TabBar标签视图控制器/AppDelegate.m
@@ -20,10 +20,63 @@
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
-    NSArray *controllers = [NSArray array];
+    
+    /*
+     1. 导航试图控制器和标签试图控制器的区别
+     （1.0）导航控制器，管理的控制器之间存在层级关系（依赖关系）
+     （1.1）标签控制器， 管理的控制器之间存在并列关系（同时存在，互不干扰）
+     2.0 管理方式
+     导航视图控制器以栈的形式管理多个试图控制器，push 入栈， POP出栈，当返回上一届面时，空间收回
+     标签视图，以不可变数组管理，而且创建时必须要全部制定上所管理的多个试图控制器，而且多个试图控制器同时存在，空间不会回收
+     3.0 视图之间的嵌套
+     正常用法：标签视图控制器管理多个导航控制器，每个导航控制器又管理多个单试图控制器。（每个界面都会有导航条，而且都是不同的，单试图控制器可以自定义导航内容）
+     非正常用法：导航控制器管理标签控制器，标签控制器用来管理多个单试图控制器。（每个界面都会有导航条， 但是导航条都是一样的，单试图控制器不可以自定义导航条的内容）这种非正常方式不符合逻辑，基本上不这么用
+     4.0 导航控制器 标签控制器  都是多视图控制器，可以管理多个视图控制器， 但是UITableViewController，UIViewController是单试图控制器，不可以管理试图控制器，只能管理视图
+     */
+    
+    FirstViewController *firstVC = [[FirstViewController alloc]init];
+    //设置一个导航条控制器
+    UINavigationController *firstNV = [[UINavigationController alloc]initWithRootViewController:firstVC];
+    
+    
+    SecondViewController *secondVC = [[SecondViewController alloc]init];
+    
+    ThirdViewController *thirdVC = [[ThirdViewController alloc]init];
+    
+    FourthViewController *fourthVC = [[FourthViewController alloc]init];
+    
+    FifthViewController *fifthVC = [[FifthViewController alloc]init];
+    
+    SixthViewController *sixthVC = [[SixthViewController alloc]init];
+    
+    //给控制器设置title
+    firstNV.tabBarItem.title = @"firstNV";
+    secondVC.tabBarItem.title = @"secondVC";
+    thirdVC.tabBarItem.title = @"thirdVC";
+    fourthVC.tabBarItem.title = @"fourthVC";
+    fifthVC.tabBarItem.title = @"fifthVC";
+    sixthVC.tabBarItem.title = @"sixthVC";
+    
+    //给控制器设置图标
+    firstNV.tabBarItem.image = [UIImage imageNamed:@"ChatList_Contact_Icon_HL.tiff"];
+    
+    
+    NSArray *controllers = @[firstNV , secondVC , thirdVC , fourthVC , fifthVC , sixthVC];
     
     UITabBarController *tabarVCS = [[UITabBarController alloc]init];
     tabarVCS.viewControllers = controllers;
+    
+    self.window.rootViewController = tabarVCS;
+    
+    [firstVC release];
+    [secondVC release];
+    [thirdVC release];
+    [fourthVC release];
+    [fifthVC release];
+    [sixthVC release];
+    [firstNV release];
+    
+    
     
     
     
